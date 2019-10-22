b6954911fdc63842c08362ff8da23a4dddbe2781
adriancleung <adrian.leung@ualberta.ca>
2019-10-21 19:50:23 -0600

    Added profile layout and user class

---
+++ app/src/main/java/com/example/vibes/User.java
@@ -0,0 +1,22 @@
+package com.example.vibes;
+
+public class User {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String username, String firstName, String lastName, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public User(String username) {
+        this.username = username;
+        /*
+        Initialize with values from firebase
+         */
+    }
+}
