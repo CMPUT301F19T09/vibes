cbf9875f28b554caa3a8e330ee919d5457235f85
Grayson Germsheid <germshei@ualberta.ca>
2019-10-27 17:21:29 -0600

    Created layouts, working on implementation

---
+++ app/src/main/java/com/cmput301f19t09/vibes/MoodListFragment.java
@@ -0,0 +1,48 @@
+package com.cmput301f19t09.vibes;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoodListFragment extends Fragment
+{
+    private ArrayAdapter<MoodEvent> adapter;
+    private List<MoodEvent> data;
+    private final MainActivity parent = (MainActivity) getActivity();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        ListView view = parent.findViewById(R.id.mood_listview);
+
+        view.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> self, View view, int position, long id)
+            {
+                Bundle arguments = new Bundle();
+
+                MoodEvent event = (MoodEvent) self.getItemAtPosition(position);
+
+                arguments.putSerializable("mood", event);
+
+                ((MainActivity) parent).replaceFragment(AddFragment.class, arguments);
+            }
+        });
+
+        data = new ArrayList<MoodEvent>();
+        adapter = new MoodAdapter(getContext(), data);
+    }
+}
