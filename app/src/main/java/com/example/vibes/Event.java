7bacfbb5380491c0032446aa5df8674265beb7a4
TaylorZowtuk <zowtukt@gmail.com>
2019-10-27 14:56:05 -0600

    Implemented getters and setters

--- app/src/main/java/com/example/vibes/Event.java
+++ app/src/main/java/com/example/vibes/Event.java
@@ -1,43 +1,97 @@
 package com.example.vibes;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+/**
+ * TODO
+ *
+ * @see something?
+ */
 public class Event {
-    LocalDateTime dateTime;
+    LocalDate date; // format yyyy-MM-dd
+    LocalTime time; // format HH:mm
+    final static String TIME_FORMAT = "HH:mm";
     String description;
 
-    public Event(LocalDateTime dateTime, String description) {
-        this.dateTime = dateTime;
+    public Event(LocalDate date, LocalTime time, String description) {
+        this.date = date;
+        this.time = time;
         this.description = description;
     }
 
     /**
-     * Getter for the dateTime attribute.
+     * Getter for the date attribute.
      *
-     * @return  dateTime
-     *      LocalDateTime object representing the date and time an event occurred
+     * @return  date
+     *      LocalDate object representing the date an event occurred.
+     *      Has the format yyyy-MM-dd.
      */
-    public LocalDateTime getDateTime() {
-        return dateTime;
+    public LocalDate getDate() {
+        return date;
     }
 
     /**
-     * Sets the dateTime attribute. DateTime is used to indicate when an event
-     * occurred. An Event can have only one dateTime.
+     * Sets the date attribute. Date is used to indicate in part when an event
+     * occurred. An Event can have only one date.
      *
-     * @param   dateTime
-     *      when an Event occurred.
+     * @param   date
+     *      Date of when an Event occurred. Must have the format yyyy-MM-dd or will not be accepted.
+     * @return
+     *      True if date was set correctly, otherwise False.
      */
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
+    public Boolean setDate(String date) {
+        try {
+            LocalDate formattedDate = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
+
+            // if we get here then format of date was correct
+            this.date = formattedDate;
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Getter for the time attribute.
+     *
+     * @return  time
+     *      LocalTime object representing the time an event occurred.
+     *      Has the format HH:mm.
+     */
+    public LocalTime getTime() {
+        return time;
+    }
+
+    /**
+     * Sets the time attribute. Time is used to indicate in part when an event
+     * occurred. An Event can have only one date.
+     *
+     * @param   time
+     *      Time of when an Event occurred. Must have the format HH:mm or will not be accepted.
+     * @return
+     *      True if time was set correctly, otherwise False.
+     */
+    public boolean setTime(String time) {
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern(TIME_FORMAT);
+            LocalTime formattedTime = LocalTime.parse(time, dtf);
+
+            // if we get here then format of time was correct
+            this.time = formattedTime;
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 
     /**
      * Getter for the description attribute.
      *
      * @return  description
-     *      description describes the details of an Event
+     *      Description describes the details of an Event.
      */
     public String getDescription() {
         return description;
@@ -48,8 +102,8 @@ public class Event {
      * which gives context to an event.
      *
      * @param   description
-     *      description must be a String and should provide details which give
-     *      context to an Event
+     *      Description must be a String and should provide details which give
+     *      context to an Event.
      */
     public void setDescription(String description) {
         this.description = description;
