934d2feeef161778a9af49e99a172d90cc670e63
Fatih <akfatih2@gmail.com>
2019-10-27 13:17:21 -0600

    Downloaded Google mapview

---
+++ app/src/main/java/com/cmput301f19t09/vibes/MapFragment.java
@@ -0,0 +1,17 @@
+package com.cmput301f19t09.vibes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MapFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+}
