e0fd2a8324857a061a5f4fb516f0975aa0713d3e
Fatih <akfatih2@gmail.com>
2019-10-27 23:03:18 -0600

    Took the main screen to be the same as devel's screen

--- app/src/main/java/com/cmput301f19t09/vibes/MainActivity.java
+++ app/src/main/java/com/cmput301f19t09/vibes/MainActivity.java
@@ -1,8 +1,6 @@
 package com.cmput301f19t09.vibes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -12,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        MapFragment fragment = new MapFragment();
-        fragmentTransaction.add(R.id.map_container, fragment);
-        fragmentTransaction.commit();
     }
 }
