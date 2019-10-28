4ffaad149f32f97db6716a612e7d0218771fd864
Fatih <akfatih2@gmail.com>
2019-10-27 22:49:41 -0600

    Fixed the structure for OOP

---
+++ app/src/main/java/com/cmput301f19t09/vibes/mapfragment/MapData.java
@@ -0,0 +1,8 @@
+package com.cmput301f19t09.vibes.mapfragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MapData extends ArrayList<UserPoint> {
+
+}
--- app/src/main/java/com/cmput301f19t09/vibes/mapfragment/MapFragment.java
+++ app/src/main/java/com/cmput301f19t09/vibes/mapfragment/MapFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -25,10 +26,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.*;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    ArrayList<UserPoint> data;
+    GoogleMap googlemap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -38,6 +42,33 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            UserPoint mapPoint = (UserPoint)bundle.getSerializable("MapPoint");
+            if(mapPoint != null){
+                this.data = new ArrayList<UserPoint>();
+                this.data.add(mapPoint);
+            }
+        }
+    }
+
+    public void showUserPoint(UserPoint point){
+        if(googlemap != null){
+            MarkerOptions options = new MarkerOptions();
+            options.position(point.location);
+            if(point.reason!=null){
+                options.snippet(point.reason);
+            }
+            if(point.emotion != null){
+                options.title(point.emotion);
+            }
+            switch(point.emotion) {
+                case "HAPPY":
+                    options.icon(bitmapDescriptorFromVector(getActivity(),R.drawable.happy));
+                    break;
+            }
+            googlemap.addMarker(options);
+        }
     }
 
     @Override
@@ -53,13 +84,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     /**
      * User structure required to display a user.
      */
-    static class UserPoint{
+    static class UserPoint implements Serializable {
         String username;
-        double lon;
-        double lat;
         LatLng location;
         int moodId;
-    }
+        String emotion;
+        String reason;
+}
 
     /**
      * Showing a user's mood
@@ -74,46 +105,51 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
      * Returns a mock user for dev purposes.
      * @return
      */
-    public UserPoint getMockUser(){
+    public static UserPoint getMockUser(){
         UserPoint mockUserPoint = new UserPoint();
         mockUserPoint.username = "testuser";
-        mockUserPoint.lon = 113.4938;
-        mockUserPoint.lat = 53.5461;
         mockUserPoint.location = new LatLng(53.5461, 113.4938);
         mockUserPoint.moodId = 0;
+        mockUserPoint.emotion = "HAPPY";
+        mockUserPoint.reason = "I am pregnant";
         return mockUserPoint;
     }
 
     @Override
     public void onMapReady(GoogleMap mMap) {
-        Log.d("DEBUG", "Map ready");
+        googlemap = mMap;
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         mMap.clear(); //clear old markers
 
-        CameraPosition googlePlex = CameraPosition.builder()
-                .target(new LatLng(37.4219999,-122.0862462))
-                .zoom(10)
-                .bearing(0)
-                .tilt(45)
-                .build();
+//        CameraPosition googlePlex = CameraPosition.builder()
+//                .target(new LatLng(37.4219999,-122.0862462))
+//                .zoom(10)
+//                .bearing(0)
+//                .tilt(45)
+//                .build();
+//
+//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
 
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(37.4219999, -122.0862462))
-                .title("Spider Man")
-                .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
-
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(37.4629101,-122.2449094))
-                .title("Iron Man")
-                .snippet("His Talent : Plenty of money"));
-
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(37.3092293,-122.1136845))
-                .title("Captain America"));
+            if(this.data != null){
+                    for (UserPoint p:this.data) {
+                        this.showUserPoint(p);
+                    }
+        }
+//        mMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(37.4219999, -122.0862462))
+//                .title("Spider Man")
+//                .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.happy)));
+//
+//        mMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(37.4629101,-122.2449094))
+//                .title("Iron Man")
+//                .snippet("His Talent : Plenty of money"));
+//
+//        mMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(37.3092293,-122.1136845))
+//                .title("Captain America"));
     }
 
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
