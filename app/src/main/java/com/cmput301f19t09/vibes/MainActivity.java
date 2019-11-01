3a2652c7faf4062159a9291fdc1c61f84b6123a9
Fatih <akfatih2@gmail.com>
2019-11-01 16:34:42 -0600

    Little fixes

--- app/src/main/java/com/cmput301f19t09/vibes/MainActivity.java
+++ app/src/main/java/com/cmput301f19t09/vibes/MainActivity.java
@@ -16,13 +16,14 @@ import com.cmput301f19t09.vibes.fragments.mapfragment.MapData;
 import com.cmput301f19t09.vibes.fragments.mapfragment.MapFragment;
 import com.cmput301f19t09.vibes.fragments.mapfragment.UserPoint;
 
-public class MainActivity extends FragmentActivity {
-    //private final static Class defaultFragment = MoodListFragment.class;
 
-    private enum ButtonMode {
-        LIST,
-        MAP;
-    }
+/**
+ * So here is how things work.
+ * You press a button
+ */
+public class MainActivity extends FragmentActivity {
+
+    private enum ButtonMode {LIST, MAP}
 
     private ButtonMode currentButtonMode;
     private String username;
