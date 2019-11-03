ef4a6c7fd1879959e7e8d5bdf7ae5e02829452d3
Fatih <akfatih2@gmail.com>
2019-11-02 20:56:22 -0600

    Restructing comments and the mood class

--- app/src/main/java/com/cmput301f19t09/vibes/models/Mood.java
+++ app/src/main/java/com/cmput301f19t09/vibes/models/Mood.java
@@ -13,29 +13,9 @@ public class Mood {
     private int day;
     private int hour;
     private int minute;
+
+    // I added the location and the reason - fatih
     private String reason;
-
-    public String getReason() {
-        if(this.reason == null){
-            return "";
-        }
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-
-
-    public GeoPoint getLocation() {
-        return location;
-    }
-
-    public void setLocation(GeoPoint location) {
-        this.location = location;
-    }
-
     private GeoPoint location;
 
     public Mood(String name, String emotionName, int year, int month, int day, int hour, int minute) {
@@ -56,6 +36,25 @@ public class Mood {
         this.day = day;
         this.hour = hour;
         this.minute = minute;
+        this.location = location;
+    }
+
+    public String getReason() {
+        if(this.reason == null){
+            return "";
+        }
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
         this.location = location;
     }
 
@@ -87,11 +86,19 @@ public class Mood {
         return this.minute;
     }
 
+    /**
+     * Returns the string format of the mood
+     * @return
+     */
     @Override
     public String toString(){
         return this.name+":"+this.emotion+","+this.year+","+this.month+","+this.day;
     }
 
+    /**
+     * Returns the string of the emotion
+     * @return
+     */
     public String getStringEmotion(){
         return this.emotion.getEmotion();
     }
