289acea33716161a88f08d085c20cc53630dd45a
adriancleung <adrian.leung@ualberta.ca>
2019-11-05 18:46:49 -0700

    Added MoodListFragment to ProfileFragment

--- app/src/main/java/com/cmput301f19t09/vibes/models/User.java
+++ app/src/main/java/com/cmput301f19t09/vibes/models/User.java
@@ -21,11 +21,8 @@ import com.google.firebase.storage.StorageReference;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -105,6 +102,8 @@ public class User implements Serializable {
         this.lastName = lastName;
         this.email = email;
         this.picturePath = "image/" + this.userName + ".png";
+
+        
     }
 
     /**
