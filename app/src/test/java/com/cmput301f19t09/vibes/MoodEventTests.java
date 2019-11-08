d4fa52bd64237a0dceda35f871f2353054299db4
TaylorZowtuk <zowtukt@gmail.com>
2019-11-08 01:27:53 -0700

    Implemented unit test and fixed comapreto method of MoodEvent

---
+++ app/src/test/java/com/cmput301f19t09/vibes/MoodEventTests.java
@@ -0,0 +1,36 @@
+package com.cmput301f19t09.vibes;
+import android.location.Location;
+
+import com.cmput301f19t09.vibes.models.EmotionalState;
+import com.cmput301f19t09.vibes.models.MoodEvent;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class MoodEventTests {
+
+    MoodEvent m1, m2;
+
+    @BeforeEach
+    private void mockMoodEvents() {
+        LocalDate ld = LocalDate.now();
+        LocalTime lt = LocalTime.now();
+        m1 = new MoodEvent(ld, lt, "", null, 0, null, null);
+
+        ld = LocalDate.now();
+        lt = LocalTime.now();
+        m2 = new MoodEvent(ld, lt, "", null, 1, null, null);
+    }
+
+    @Test
+    void testCompareTo() {
+        // compareTo returns -1 if m1's LocalDateTime is before m2's
+        assertEquals(-1, m1.compareTo(m2));
+        // compareTo returns 1 if m2's LocalDateTime is after m1's
+        assertEquals(1, m2.compareTo(m1));
+    }
+}
