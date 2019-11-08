e1cc93db879635ae7de85dc4c967e779763464d4
adriancleung <adrian.leung@ualberta.ca>
2019-11-07 23:02:42 -0700

    Modified to fix constraints

--- app/src/main/java/com/cmput301f19t09/vibes/fragments/profilefragment/ProfileFragment.java
+++ app/src/main/java/com/cmput301f19t09/vibes/fragments/profilefragment/ProfileFragment.java
@@ -16,7 +16,6 @@ import com.cmput301f19t09.vibes.fragments.mooddetailsfragment.MoodDetailsFragmen
 import com.cmput301f19t09.vibes.fragments.moodlistfragment.MoodListFragment;
 import com.cmput301f19t09.vibes.models.User;
 import com.cmput301f19t09.vibes.models.UserManager;
-import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -26,8 +25,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class ProfileFragment extends Fragment implements Observer {
     public static final String PROFILE_FRAGMENT_TAG = "ProfileFragment";
-    private TextView firstNameTextView;
-    private TextView lastNameTextView;
+    private TextView fullNameTextView;
     private TextView userNameTextView;
     private ImageView profilePictureImageView;
     private Button followButton;
@@ -58,8 +56,7 @@ public class ProfileFragment extends Fragment implements Observer {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_profile, container, false);
 
-        firstNameTextView = view.findViewById(R.id.firstname_textview);
-        lastNameTextView = view.findViewById(R.id.lastname_textview);
+        fullNameTextView = view.findViewById(R.id.fullname_textview);
         userNameTextView = view.findViewById(R.id.username_textview);
         profilePictureImageView = view.findViewById(R.id.profile_picture);
         followButton = view.findViewById(R.id.follow_button);
@@ -122,8 +119,7 @@ public class ProfileFragment extends Fragment implements Observer {
      */
     public void setInfo(User user) {
         if (user.isLoaded()) {
-            firstNameTextView.setText(user.getFirstName());
-            lastNameTextView.setText(user.getLastName());
+            fullNameTextView.setText(user.getFirstName() + " " + user.getLastName());
             userNameTextView.setText(user.getUserName());
             Glide.with(this).load(user.getProfileURL()).into(profilePictureImageView);
             profilePictureImageView.setClipToOutline(true);
