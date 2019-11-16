9d0179f390f4510280a76cfffaa6b2364c789f47
Fatih <akfatih2@gmail.com>
2019-11-15 21:54:34 -0700

    Fixed the issue, added a method for cleaning up

--- app/src/main/java/com/cmput301f19t09/vibes/fragments/mapfragment/MapFragment.java
+++ app/src/main/java/com/cmput301f19t09/vibes/fragments/mapfragment/MapFragment.java
@@ -1,12 +1,10 @@
 package com.cmput301f19t09.vibes.fragments.mapfragment;
 
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -27,9 +25,7 @@ import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.ClusterRenderer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -53,7 +49,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
 
     Filter filter;
     private List<String> observedUsers;
-    private Map<String, MoodEvent> displayedEvents;
 
     /**
      * The map fragment shows the locations of the moods.
@@ -63,7 +58,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
     public MapFragment() {
         // Required empty public constructor
         observedUsers = new ArrayList<>();
-        displayedEvents = new HashMap<>();
     }
 
     public static MapFragment newInstance(){
@@ -83,19 +77,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
      * Displays the MoodEvent in the map.
      * @param event The MoodEvent that you're trying to show.
      */
-    public String showMoodEvent(MoodEvent event){
-        String id = "";
+    public void showMoodEvent(MoodEvent event){
         if(googlemap != null){
             // Adding the event to the cluster manager
             mClusterManager.addItem(event);
-
+            mClusterManager.cluster();
             // If first marker, move the camera to the marker
             if(!firstPointPut ){
                 firstPointPut = true;
                 googlemap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(event.getLocation().getLatitude(), event.getLocation().getLongitude()), 4));
             }
         }
-        return id;
     }
 
     /**
@@ -116,7 +108,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
         mapFragment.getMapAsync(this);
         getChildFragmentManager().beginTransaction().add(R.id.filter_root, MapFilter.getInstance(Filter.SHOW_MINE), "mapFilter").commit();
         UserManager.addUserObserver(UserManager.getCurrentUserUID(), this);
-
         return view;
     }
 
@@ -140,6 +131,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
         mClusterManager.setOnClusterInfoWindowClickListener(this);
         mClusterManager.setOnClusterItemClickListener(this);
         mClusterManager.setOnClusterItemInfoWindowClickListener(this);
+
         // Point the map's listeners at the listeners implemented by the cluster
         // manager.
         googlemap.setOnCameraIdleListener(mClusterManager);
@@ -150,17 +142,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
     }
 
     public void switchFilter(Filter filter) {
-        if (googlemap == null)
+        if (googlemap == null) {
+            Log.e("SWITCH FILTER", "NO GOOGLEMAP DEFINED");
             return;
+        }
 
         this.filter = filter;
         User user = UserManager.getCurrentUser();
-        Log.d("TEST/Map", "Showing own events");
+        clusterCleanUp();
         if (filter == Filter.SHOW_MINE) {
+            Log.d("TEST/Map", "Showing own events");
             removeObservers();
             showUserEvents();
         } else if (filter == Filter.SHOW_EVERYONE) {
-            Log.d("TEST/Map", "Showing other events");
+            Log.d("TEST/Map", "Showing everyone's events");
             for (String id : user.getFollowingList())
             {
                 Log.d("TEST/Map", "Adding " + id);
@@ -178,12 +173,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
                     }
                 });
             }
+        }else{
+            throw new RuntimeException("Map filter is not recognized.");
         }
     }
 
     private void showFollowedEvents()
     {
-        googlemap.clear();
         for (String id : observedUsers)
         {
             User user = UserManager.getUser(id);
@@ -196,8 +192,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
 
     private void showUserEvents()
     {
-        googlemap.clear();
-        displayedEvents.clear();
         for (MoodEvent event : UserManager.getCurrentUser().getMoodEvents()) {
             showEvent(event);
         }
@@ -205,12 +199,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
 
     private void showEvent(MoodEvent event)
     {
-        String id = showMoodEvent(event);
-
-        if (id != "")
-        {
-            displayedEvents.put(id, event);
-        }
+        showMoodEvent(event);
     }
 
     private void removeObservers()
@@ -228,9 +217,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
         super.onPause();
     }
 
+    public void clusterCleanUp(){
+        mClusterManager.clearItems();
+        mClusterManager.cluster();
+        Log.d("TEST/Map", "Cleared the cluster items");
+    }
+
     @Override
     public void update(Observable o, Object arg) {
         Log.d("TEST/Map", "Current User Update");
+        clusterCleanUp();
         if (filter == Filter.SHOW_MINE)
         {
             showUserEvents();
@@ -261,25 +257,24 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Observe
     }
 
     @Override
-    public boolean onClusterClick(Cluster<MoodEvent> event) {
-        Log.d("MAP", "cluster is clicked.");
+    public boolean onClusterClick(Cluster<MoodEvent> events) {
+        Log.d("MAP", "Cluster has " + events.getSize() + " events.");
         return true;
     }
 
     @Override
-    public void onClusterInfoWindowClick(Cluster<MoodEvent> event) {
+    public void onClusterInfoWindowClick(Cluster<MoodEvent> events) {
         Log.d("MAP", "cluster info is clicked.");
     }
 
     @Override
     public boolean onClusterItemClick(MoodEvent event) {
-        ((MainActivity)getActivity()).openDialogFragment(MoodDetailsDialogFragment.newInstance(event, filter == Filter.SHOW_MINE));
-        Toast.makeText(getActivity(), "NOONONO",Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     public void onClusterItemInfoWindowClick(MoodEvent event) {
+        ((MainActivity)getActivity()).openDialogFragment(MoodDetailsDialogFragment.newInstance(event, filter == Filter.SHOW_MINE));
         Log.d("MAP", "clusterPoint info is clicked.");
     }
 }
