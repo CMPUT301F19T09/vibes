6ec3aa3c7f609ec91234950a796c9bd0631379ec
Fatih <akfatih2@gmail.com>
2019-11-15 18:23:40 -0700

    The icons can show the emoticons now

--- app/src/main/java/com/cmput301f19t09/vibes/fragments/mapfragment/CustomClusterRenderer.java
+++ app/src/main/java/com/cmput301f19t09/vibes/fragments/mapfragment/CustomClusterRenderer.java
@@ -1,15 +1,58 @@
 package com.cmput301f19t09.vibes.fragments.mapfragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 
+import androidx.core.content.ContextCompat;
+
+import com.cmput301f19t09.vibes.models.EmotionalState;
+import com.cmput301f19t09.vibes.models.MoodEvent;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.ClusterRenderer;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 
-public class CustomClusterRenderer extends DefaultClusterRenderer {
+public class CustomClusterRenderer extends DefaultClusterRenderer<MoodEvent> {
+    private IconGenerator mIconGenerator;
+    private IconGenerator mClusterIconGenerator;
+    private Context context;
     public CustomClusterRenderer(Context context, GoogleMap map, ClusterManager clusterManager) {
         super(context, map, clusterManager);
+        this.context = context;
+        mIconGenerator = new IconGenerator(context);
         this.setMinClusterSize(2);
     }
+
+
+    @Override
+    protected void onBeforeClusterItemRendered(MoodEvent event, MarkerOptions markerOptions) {
+//        mImageView.setImageResource(person.profilePhoto);
+        Bitmap icon = mIconGenerator.makeIcon();
+
+
+        Integer emoticon = (Integer) EmotionalState.getMap().get(event.getState().getEmotion()).first;
+        Integer color = (Integer)  EmotionalState.getMap().get(event.getState().getEmotion()).second;
+        markerOptions.icon(bitmapDescriptorFromVector(this.context, emoticon, color));
+//        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title(person.name);
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId, Integer color) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        vectorDrawable.setBounds(0, 0, 100, 100);
+
+        Bitmap bitmap2 = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = (Bitmap.createScaledBitmap(bitmap2, 100, 100, true));
+
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
 }
