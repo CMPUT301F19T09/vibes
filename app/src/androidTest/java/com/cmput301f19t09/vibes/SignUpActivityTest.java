c1008a6ea7f3a6f655ec33c45e780dfb28db144e
TaylorZowtuk <zowtukt@gmail.com>
2019-11-21 19:11:14 -0700

    Added navigate to signup from login test

--- app/src/androidTest/java/com/cmput301f19t09/vibes/SignUpActivityTest.java
+++ app/src/androidTest/java/com/cmput301f19t09/vibes/SignUpActivityTest.java
@@ -1,20 +1,26 @@
 package com.cmput301f19t09.vibes;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.rule.ActivityTestRule;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import io.victoralbertos.device_animation_test_rule.DeviceAnimationTestRule;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class SignUpActivityTest {
@@ -23,18 +29,34 @@ public class SignUpActivityTest {
 
     final private String USER_COLLECTION = "users";
 
-    @Rule
-    public IntentsTestRule<SignUpActivity> intentsTestRule =
-            new IntentsTestRule<>(SignUpActivity.class);
+    // automate disabling device animations which is required by espresso
+    @ClassRule
+    public static DeviceAnimationTestRule
+            deviceAnimationTestRule = new DeviceAnimationTestRule();
 
-    @Before
-    public void initializeConnection() {
-        mAuth = FirebaseAuth.getInstance();
-        mStore = FirebaseFirestore.getInstance();
+    @Rule
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, true, true);
+
+//    @Before
+//    public void initializeConnection() {
+//        mAuth = FirebaseAuth.getInstance();
+//        mStore = FirebaseFirestore.getInstance();
+//    }
+
+    /**
+     * Navigate to the SignUpActivity by clicking sign up button. Check if the fragment is loaded.
+     */
+    @Test
+    public void navigateToSignUp() {
+        onView(withId(R.id.login_activity)).check(matches(isDisplayed()));
+        onView(withId(R.id.sign_up_button)).perform(click());
+        onView(withId(R.id.signup_activity)).check(matches(isDisplayed()));
     }
 
     @Test
     public void signUpUser() throws InterruptedException {
+        navigateToSignUp();
         onView(withId(R.id.username_field)).perform(typeText("AutomatedTestUser"));
         onView(withId(R.id.email_field)).perform(typeText("automatedtestuser@example.com"));
         onView(withId(R.id.first_name_field)).perform(typeText("AutomatedTestFN"));
