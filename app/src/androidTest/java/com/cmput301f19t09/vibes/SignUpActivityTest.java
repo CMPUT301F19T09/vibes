cdfda15eed2eba2ea12f42094448b3f167f3b2b7
TaylorZowtuk <zowtukt@gmail.com>
2019-11-21 20:04:38 -0700

    Signup works but after each test need to delete auth, collection, and storage related to the signup user

--- app/src/androidTest/java/com/cmput301f19t09/vibes/SignUpActivityTest.java
+++ app/src/androidTest/java/com/cmput301f19t09/vibes/SignUpActivityTest.java
@@ -1,15 +1,20 @@
 package com.cmput301f19t09.vibes;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.cmput301f19t09.vibes.models.UserManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import io.victoralbertos.device_animation_test_rule.DeviceAnimationTestRule;
 
@@ -23,6 +28,7 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+@RunWith(AndroidJUnit4.class)
 public class SignUpActivityTest {
     private FirebaseAuth mAuth;
     private FirebaseFirestore mStore;
@@ -57,17 +63,41 @@ public class SignUpActivityTest {
     @Test
     public void signUpUser() throws InterruptedException {
         navigateToSignUp();
-        onView(withId(R.id.username_field)).perform(typeText("AutomatedTestUser"));
-        onView(withId(R.id.email_field)).perform(typeText("automatedtestuser@example.com"));
-        onView(withId(R.id.first_name_field)).perform(typeText("AutomatedTestFN"));
-        onView(withId(R.id.last_name_field)).perform(typeText("AutomatedTestLN"));
-        onView(withId(R.id.password_field)).perform(typeText("automatedtest"));
-        onView(withId(R.id.confirm_password_field)).perform(typeText("automatedtest"));
+        onView(withId(R.id.username_field)).perform(typeText("signupuser"));
+        onView(withId(R.id.email_field)).perform(typeText("signup@example.com"));
+        onView(withId(R.id.first_name_field)).perform(typeText("SignupFN"));
+        onView(withId(R.id.last_name_field)).perform(typeText("SignupLN"));
+        onView(withId(R.id.password_field)).perform(typeText("000000"));
+        closeSoftKeyboard();
+        onView(withId(R.id.confirm_password_field)).perform(typeText("000000"));
         closeSoftKeyboard();
         onView(withId(R.id.sign_up_button)).perform(click());
 
         Thread.sleep(5000);
-        intended(hasComponent(LoginActivity.class.getName()));
+        onView(withId(R.id.login_activity)).check(matches(isDisplayed()));
+
+        Login.setUp("signup@example.com", "000000");
+//        FirebaseAuth.getInstance().deleteUser(UserManager.getCurrentUserUID());
     }
+
+//    @After
+//    public void cleanUp() {
+//        FirebaseAuth.getInstance().deleteUser(uid);
+
+//        db.collection("cities").document("DC")
+//            .delete()
+//            .addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void aVoid) {
+//                Log.d(TAG, "DocumentSnapshot successfully deleted!");
+//            }
+//        })
+//                .addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.w(TAG, "Error deleting document", e);
+//            }
+//        });
+//    }
 
 }
