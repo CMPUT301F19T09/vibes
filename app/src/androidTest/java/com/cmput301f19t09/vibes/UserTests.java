99f75ed3ee77803a7eaf427443dbe48fef199d4b
David Herczeg <david.herczeg@live.ca>
2019-11-29 15:48:13 -0700

    Minor fixes for intent tests on pixel 3a

--- app/src/androidTest/java/com/cmput301f19t09/vibes/UserTests.java
+++ app/src/androidTest/java/com/cmput301f19t09/vibes/UserTests.java
@@ -356,7 +356,7 @@ public class UserTests {
         // create a new MoodEvent
         // select ANGER
         onView(withTagValue(is("ANGER"))).perform(click());
-        onView(withText("By Myself")).perform(setChecked(true));
+        onView(withText("By myself")).perform(setChecked(true));
 //        onView(withId(R.id.edit_situation_view)).perform(typeText("0.0"));
         closeSoftKeyboard();
         onView(withId(R.id.edit_reason_view)).perform(typeText("three words desc"));
@@ -378,7 +378,7 @@ public class UserTests {
         // verify that the fields are populated correctly
         onView(withId(R.id.edit_reason_view)).check(matches(withText("three words desc")));
         Thread.sleep(100);
-        onView(withText("By Myself")).check(matches(isChecked()));
+        onView(withText("By myself")).check(matches(isChecked()));
 //        onView(withId(R.id.social_chip_group)).check(matches(isNotChecked()));
         onView(withText("Anger")).check(matches(isChecked()));
 //        onView(withId(R.id.state_text_view)).check(matchesChecks());
@@ -387,7 +387,7 @@ public class UserTests {
         Thread.sleep(100);
         onView(withTagValue(is("HAPPINESS"))).perform(click());
 //        onData(anything()).inAdapterView(withId(R.id.state_grid_view)).atPosition(5).perform(click());
-        onView(withText("With Someone Else")).perform(setChecked(true));
+        onView(withText("With someone else")).perform(setChecked(true));
 //        onView(withId(R.id.edit_situation_view)).perform(typeText("1.0"));
         closeSoftKeyboard();
         onView(withId(R.id.edit_reason_view)).perform(replaceText("a new desc"));
@@ -413,7 +413,7 @@ public class UserTests {
 
         // verify that the fields are populated correctly again
         onView(withId(R.id.edit_reason_view)).check(matches(withText("a new desc")));
-        onView(withText("With Someone Else")).check(matches(isChecked()));
+        onView(withText("With someone else")).check(matches(isChecked()));
 //        onView(withId(R.id.edit_situation_view)).check(matches(withText("1.0")));
         onView(withText("Happiness")).check(matches(isChecked()));
 //        onView(withId(R.id.state_text_view)).check(matches(withText("HAPPINESS")));
